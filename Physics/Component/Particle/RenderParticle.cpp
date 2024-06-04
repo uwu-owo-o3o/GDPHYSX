d176eb7a5a0840dcffc5dea9e20366d7a6fbe2c3
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-06-04 23:32:57 +0800

    initial push

--- Physics/Component/Particle/RenderParticle.cpp
+++ Physics/Component/Particle/RenderParticle.cpp
@@ -10,6 +10,12 @@ RenderParticle::RenderParticle(Particle* pParticle, Model3D* pModel3D, Vector Co
 
 void RenderParticle::Draw() {
 	if (!this->pReferenceParticle->checkIfDestroyed()) {
+		auto modelShader = ShaderManager::getInstance()->getShader("Sphere");
+		unsigned int colorLoc = glGetUniformLocation(*modelShader, "ParticleColor");
+		//std::cout << "Color x: " << this->CColor.getX() << std::endl;
+		glm::vec4 color = glm::vec4(this->CColor.getX(), this->CColor.getY(), this->CColor.getZ(), 1.0f);
+		glUniform4fv(colorLoc, 1, glm::value_ptr(color));
+
 		this->pReferenceModel->getTransform()->setAtt(TransformAtt::TRANSLATE, this->pReferenceParticle->getPosition()->getCoordinates());
 		this->pReferenceModel->draw("Sphere");
 	}
--- Shaders/unlit.frag
+++ Shaders/unlit.frag
@@ -1,8 +1,10 @@
 #version 330 core
 
 out vec4 FragColor;
+uniform vec4 ParticleColor;
 
 void main()
 {
-    FragColor = vec4(0.5f, 0.0f, 0.0f, 1.0f);
+    //FragColor = vec4(0.5f, 0.0f, 0.0f, 1.0f);
+    FragColor = ParticleColor;
 }
